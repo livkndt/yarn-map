9d14a8a851d79e00e2cc50b7af6589c89b1a8159
Olivia Knoedt <livvy@mac.com>
2025-12-08 11:42:33 +0000

    chore: upgrade to nextjs 15

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from '@prisma/config';
 import 'dotenv/config';
 
-export default defineConfig({
+export default {
   schema: './prisma/schema.prisma',
   datasource: {
-    provider: 'postgresql',
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || '',
   },
-});
+};
